250ed540c94a55cac7e9139c41d946c162c7a406
Mark Scala <markscala@gmail.com>
2014-11-08 13:39:19 -0600

    pages sketched

--- app/controllers/ui_controller.rb
+++ app/controllers/ui_controller.rb
@@ -1,5 +1,19 @@
 class UiController < ApplicationController
   def front
-    roender
+  end
+
+  def home
+  end
+
+  def pack_page
+  end
+
+  def new_card
+  end
+
+  def card_front
+  end
+
+  def card_back
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root to: 'ui#front'
+  get 'ui(/:action)', controller: 'ui'
 end
