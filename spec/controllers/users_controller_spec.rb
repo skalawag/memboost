e014087fb9daf3d8a6fad1650e5831b5920e1f5d
Mark Scala <markscala@gmail.com>
2014-11-09 21:09:27 -0600

    fix description

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe UsersController do
-  describe "GET index" do
+  describe "GET show" do
     it "sets @user to logged in user" do
       user = Fabricate(:user)
       get :show, id: user.id
