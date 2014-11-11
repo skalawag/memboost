34d7a5b9882bbaabac6e58bf289d4d59a4034ce4
Mark Scala <markscala@gmail.com>
2014-11-11 15:05:59 -0600

    create a new pack

--- app/controllers/packs_controller.rb
+++ app/controllers/packs_controller.rb
@@ -2,4 +2,20 @@ class PacksController < ApplicationController
   def show
     @pack = Pack.find(params[:id])
   end
+
+  def create
+    pack = Pack.new(pack_params)
+    if pack.save
+      current_user.packs << pack
+      redirect_to user_path(current_user)
+    else
+      flash[:error] = "Something went wrong there. Try again."
+    end
+  end
+
+  private
+
+  def pack_params
+    params.require(:pack).permit(:name)
+  end
 end
