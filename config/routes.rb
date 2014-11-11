d1f25931cb3ec042dbbf9d30a00aca2a379e3c40
Mark Scala <markscala@gmail.com>
2014-11-11 13:37:45 -0600

    login routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  root to: 'ui#register'
   get 'ui(/:action)', controller: 'ui'
 
-  resources :users, only: [:show, :new, :create]
+  root to: 'pages#index'
+  resources :users, only: [:show, :create]
   resources :packs, only: [:show]
   resources :cards, only: [:new, :create, :show]
   get '/answer/:card_id', to: 'cards#answer', as: :answer
   get '/learned/:card_id', to: 'cards#learned', as: :learned
   get '/random_card', to: 'cards#random_card', as: :random_card
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
 end
