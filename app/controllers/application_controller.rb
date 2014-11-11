bce797ba2bc69a27cdec98dee504e7f763bf488a
Mark Scala <markscala@gmail.com>
2014-11-11 09:15:57 -0600

    current_user and logged_in? for app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  helper_method :current_user, :logged_in?
+
   def current_user
-    User.first
+    current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
   end
 end
