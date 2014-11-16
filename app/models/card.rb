95c805bc878301a1a2fa5b34b0c6aafc24a51792
Mark Scala <markscala@gmail.com>
2014-11-15 19:18:22 -0600

    prettier dates

--- app/models/card.rb
+++ app/models/card.rb
@@ -4,4 +4,8 @@ class Card < ActiveRecord::Base
   validates_numericality_of :attempts, greater_than: -1
   validates_format_of :learning_stage, with: /(unfamiliar)|(familiar)|(learned)/
 
+  def creation_date
+    time = self.created_at.time
+    time.month.to_s + "-" + time.day.to_s + "-" + time.year.to_s
+  end
 end
